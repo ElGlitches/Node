d19e0bb59097f5c2cd317f388c8a1be7cdec2587
ElGlitches <ivanduranluengo@gmail.com>
2021-10-20 11:49:45 -0300

    .

---
+++ JavaScript/01_TiposDeDatos.js
@@ -0,0 +1,36 @@
+/** Tipos de datos */
+
+// numero - number
+console.log(123);
+console.log(1.23);
+// 
+
+// texto - string
+console.log("Hola");
+console.log('Hola');
+console.log(`Hola`);
+
+//logicos - booleans
+console.log(true);
+console.log(false);
+
+// indefinido - undefined
+console.log(undefined);
+
+//null
+console.log(null);
+
+/**
+ * tipos de datos estructurales
+ */
+
+//Objeto - object
+console.log({ nombre: 'Ivan', edad: 32 , fechaDeNacimiento: null, paises: ['Chile', 'Mexico']});
+
+// Tipo de dato arreglo  - array
+
+console.log(['Chile', 'Mexico']);
+console.log([32,true, { nombre: 'Ivan', edad: 32 , fechaDeNacimiento: null},[1,2,3,4,5,6,7]]);
+
+
+
