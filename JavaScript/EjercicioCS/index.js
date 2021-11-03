be04a75b5e0d275ccf58cd5afa9d5be89e8d8d12
ElGlitches <ivanduranluengo@gmail.com>
2021-11-02 22:57:35 -0300

    ejerciciom Rick & Morty

---
+++ JavaScript/EjercicioCS/index.js
@@ -0,0 +1,19 @@
+const axios = require("axios");
+
+const main = async() => {
+    let response = await axios.get('https://rickandmortyapi.com/api/character');
+    let{
+        data: { results },
+    } = response;
+    let personajes = results.map((personajes) => {
+        return {
+            id : personajes.id,
+            name: personajes.name,
+            species: personajes.species
+
+        }
+    })
+    console.log(personajes);
+};
+
+main();
